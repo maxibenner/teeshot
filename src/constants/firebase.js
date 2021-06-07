754f2b62d6598692d8831e0b25b0cbba78834009
Maximilian Benner <maxibenner@gmail.com>
2021-06-07 07:57:37 -1000

    reload env

--- src/constants/firebase.js
+++ src/constants/firebase.js
@@ -1,7 +1,6 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
-// Init
 const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
